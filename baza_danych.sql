5e3f5fd8bfc42afcc3ff46b96a355f80e7083cf4
i3olo <i3olo19951995@gmail.com>
2017-05-16 18:49:04 +0200

    no message

--- baza_danych.sql
+++ baza_danych.sql
@@ -1,49 +1,59 @@
 CREATE TABLE Autobus
-  ( ID NUMBER NOT NULL , Model_ID NUMBER NOT NULL
-  ) ;
+ ( 
+	ID 					NUMBER NOT NULL ,
+	Model_ID 			NUMBER NOT NULL
+ ) ;
 ALTER TABLE Autobus ADD CONSTRAINT Autobus_PK PRIMARY KEY ( ID ) ;
 
 
 CREATE TABLE Bilet
-  ( ID NUMBER NOT NULL , Rodzaj_biletu_ID NUMBER NOT NULL
-  ) ;
+( 
+	ID 					NUMBER NOT NULL , 
+	Rodzaj_biletu_ID 	NUMBER NOT NULL
+) ;
 ALTER TABLE Bilet ADD CONSTRAINT Bilet_PK PRIMARY KEY ( ID ) ;
 
 
 CREATE TABLE Data_przejazdu
-  ( ID NUMBER NOT NULL , Data DATE
-  ) ;
+( 
+	ID 					NUMBER NOT NULL ,
+	Data 				DATE
+) ;
 ALTER TABLE Data_przejazdu ADD CONSTRAINT Data_przejazdu_PK PRIMARY KEY ( ID ) ;
 
 
 CREATE TABLE Kierowca
-  (
-    ID       NUMBER NOT NULL ,
-    Imie     VARCHAR2 (30) ,
-    Nazwisko VARCHAR2 (30)
-  ) ;
+(
+    ID       			NUMBER NOT NULL ,
+    Imie     			VARCHAR2 (30) ,
+    Nazwisko 			VARCHAR2 (30)
+) ;
 ALTER TABLE Kierowca ADD CONSTRAINT Kierowca_PK PRIMARY KEY ( ID ) ;
 
 
 CREATE TABLE Linia
-  ( ID NUMBER NOT NULL , Nazwa_linii VARCHAR2 (20)
-  ) ;
+( 
+	ID 					NUMBER NOT NULL , 
+	Nazwa_linii 		VARCHAR2 (20)
+) ;
 ALTER TABLE Linia ADD CONSTRAINT Linia_PK PRIMARY KEY ( ID ) ;
 
 
 CREATE TABLE Marka
-  ( ID NUMBER NOT NULL , Nazwa VARCHAR2 (30)
-  ) ;
+( 
+	ID 					NUMBER NOT NULL ,
+	Nazwa 				VARCHAR2 (30)
+) ;
 ALTER TABLE Marka ADD CONSTRAINT Marka_PK PRIMARY KEY ( ID ) ;
 
 
 CREATE TABLE Model
-  (
-    ID           NUMBER NOT NULL ,
-    Marka_ID     NUMBER NOT NULL ,
-    Ilosc_Miejsc NUMBER ,
-    Nazwa        VARCHAR2 (30)
-  ) ;
+(
+    ID           		NUMBER NOT NULL ,
+    Marka_ID     		NUMBER NOT NULL ,
+    Ilosc_Miejsc 		NUMBER ,
+    Nazwa        		VARCHAR2 (30)
+) ;
 ALTER TABLE Model ADD CONSTRAINT Model_PK PRIMARY KEY ( ID ) ;
 
 
@@ -55,7 +65,7 @@ CREATE TABLE Przejazd
     Data_przejazdu_ID  NUMBER NOT NULL ,
     Bilet_ID           NUMBER NOT NULL ,
     Linia_ID           NUMBER NOT NULL ,
-    Przebyta_odległosc NUMBER ,
+    Przebyta_odleglosc NUMBER ,
     Cena_przejazdu     NUMBER ,
     Oblozenie          NUMBER ,
     Ilosc_osob         NUMBER
@@ -65,9 +75,9 @@ ALTER TABLE Przejazd ADD CONSTRAINT Przejazd_PK PRIMARY KEY ( ID ) ;
 
 CREATE TABLE Rodzaj_biletu
   (
-    ID             NUMBER NOT NULL ,
-    Nazwa          VARCHAR2 (30) ,
-    Procent_zniżki NUMBER
+    ID             		NUMBER NOT NULL ,
+    Nazwa          		VARCHAR2 (30) ,
+    Procent_zniżki 		NUMBER NOT NULL
   ) ;
 ALTER TABLE Rodzaj_biletu ADD CONSTRAINT Rodzaj_biletu_PK PRIMARY KEY ( ID ) ;
 
@@ -87,4 +97,3 @@ ALTER TABLE Przejazd ADD CONSTRAINT Przejazd_Data_przejazdu_FK FOREIGN KEY ( Dat
 ALTER TABLE Przejazd ADD CONSTRAINT Przejazd_Kierowca_FK FOREIGN KEY ( Kierowca_ID ) REFERENCES Kierowca ( ID ) ;
 
 ALTER TABLE Przejazd ADD CONSTRAINT Przejazd_Linia_FK FOREIGN KEY ( Linia_ID ) REFERENCES Linia ( ID ) ;
-
